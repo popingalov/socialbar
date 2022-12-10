302534adcca8a0fa9aa1af51e0f5995dbef6ffe6
Vira Voronova <vera.voronova@hotmail.com>
2022-12-10 14:01:56 +0100

    shortlayout added

--- src/Components/shortLayout/ShortLayout.tsx
+++ src/Components/shortLayout/ShortLayout.tsx
@@ -1,5 +1,17 @@
-import React from 'react';
+import { Suspense } from 'react';
+import { Outlet } from 'react-router-dom';
+import { Box } from 'Components/box/Box';
+import { Loader } from '../loader/Loader';
 
 export const ShortLayout = () => {
-  return <div>ShortLayout</div>;
+  return (
+    <>
+      <div>ShortLayout</div>
+      <Box as="main">
+        <Suspense fallback={<Loader />}>
+          <Outlet />
+        </Suspense>
+      </Box>
+    </>
+  );
 };
