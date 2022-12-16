2387d59c3421cb6744ba8ab3c240223a0968d7e2
Vira Voronova <vera.voronova@hotmail.com>
2022-12-16 18:45:36 +0100

    updated

--- src/components/appBar/AppBar.tsx
+++ src/components/appBar/AppBar.tsx
@@ -1,24 +1,5 @@
-import { MobileMenu } from 'components/mobileMenu/MobileMenu';
-import { AnimatePresence } from 'framer-motion';
-import { useState } from 'react';
 import { Box } from '../box/Box';
 
 export const AppBar = () => {
-  const [isMenuOpen, setIsMenuOpen] = useState<boolean>(false);
-  const toggleMenu = () => {
-    setIsMenuOpen(prevState => !prevState);
-  };
-
-  return (
-    <Box as="header">
-      App Bar
-      {/* temp */}
-      <button onClick={toggleMenu}>open mobile menu</button>
-      {/* to check when element will be unmounted for animation */}
-      {/* <AnimatePresence>
-        {isMenuOpen && <MobileMenu key="menu" closeMenu={toggleMenu} />}
-      </AnimatePresence> */}
-      {isMenuOpen && <MobileMenu closeMenu={toggleMenu} />}
-    </Box>
-  );
+  return <Box as="header">App Bar</Box>;
 };
