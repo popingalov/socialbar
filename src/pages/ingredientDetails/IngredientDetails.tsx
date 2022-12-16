93407736dc13cc55ae3703ce828afbb371a9559d
Oleksandr Balitskiy <popingalov@gmail.com>
2022-12-16 21:52:48 +0200

    Revert "ingredient  details"

--- src/pages/ingredientDetails/IngredientDetails.tsx
+++ src/pages/ingredientDetails/IngredientDetails.tsx
@@ -1,12 +1,3 @@
-import { useParams } from 'react-router';
-import { IngredientInfo } from 'components/ingredientInfo/IngredientInfo';
-
 export const IngredientDetails = () => {
-  const { ingredientId } = useParams();
-
-  return (
-    <div>
-      <IngredientInfo id={ingredientId} />
-    </div>
-  );
+  return <div>IngredientDetails</div>;
 };
