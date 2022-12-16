c086030250b6ba5a4bfca0b3ddc768e1642f369f
Vira Voronova <vera.voronova@hotmail.com>
2022-12-16 08:43:52 +0100

    changes

--- src/pages/ingredients/Ingredients.tsx
+++ src/pages/ingredients/Ingredients.tsx
@@ -1,9 +1,29 @@
 import { IngredientsList } from 'components/ingredientsList/IngredientsList';
 import { PagesNavigation } from 'components/pagesNavigation/PagesNavigation';
+// import { motion } from 'framer-motion';
+// import styled from 'styled-components';
+
+// const container = {
+//   hidden: { opacity: 0 },
+//   show: { opacity: 1 },
+// };
+
+// const Container = styled(motion.div)``;
 
 export const Ingredients = () => {
   return (
+    // <Container
+    //   variants={container}
+    //   initial="hidden"
+    //   animate="show"
+    //   exit="hidden"
+    //   transition={{ duration: 0.2 }}
+    // >
+    //   <PagesNavigation type="ingredients" />
+    //   <IngredientsList />
+    // </Container>
     <>
+      {' '}
       <PagesNavigation type="ingredients" />
       <IngredientsList />
     </>
