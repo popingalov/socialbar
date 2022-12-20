e733cfbe865599215224561f17cd2a4d11d7fc8a
Oleksandr Balitskiy <popingalov@gmail.com>
2022-12-20 18:07:10 +0300

    restart

--- src/service-worker.ts
+++ src/service-worker.ts
@@ -1,4 +1,12 @@
 /// <reference lib="webworker" />
+/* eslint-disable no-restricted-globals */
+
+// This service worker can be customized!
+// See https://developers.google.com/web/tools/workbox/modules
+// for the list of available Workbox modules, or add any other
+// code you'd like.
+// You can also remove this file if you'd prefer not to use a
+// service worker, and the Workbox build step will be skipped.
 
 import { clientsClaim } from 'workbox-core';
 import { ExpirationPlugin } from 'workbox-expiration';
@@ -61,22 +69,6 @@ registerRoute(
     ],
   }),
 );
-self.addEventListener('fetch', event => {
-  console.log('Event', event);
-
-  event.respondWith(
-    caches.match(event.request).then(response => {
-      if (response) {
-        console.log('Достал кеш');
-
-        return response;
-      }
-      console.log('Кеша нету');
-
-      return fetch(event.request);
-    }),
-  );
-});
 
 // This allows the web app to trigger skipWaiting via
 // registration.waiting.postMessage({type: 'SKIP_WAITING'})
