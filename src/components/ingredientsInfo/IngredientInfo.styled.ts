1be3deee3fde07e35aae7ce0fe4ce06c4d983b6c
Vira Voronova <vera.voronova@hotmail.com>
2023-01-22 15:55:28 +0100

    ingredient additional info added

--- src/components/ingredientsInfo/IngredientInfo.styled.ts
+++ src/components/ingredientsInfo/IngredientInfo.styled.ts
@@ -66,3 +66,11 @@ export const ShowMoreBtn = styled(ButtonBase)`
   margin-left: auto;
   display: block;
 `;
+
+export const AdditionalInfoTitle = styled.p`
+  padding-left: ${({ theme }) => theme.space[3]}px;
+  padding-right: ${({ theme }) => theme.space[3]}px;
+  padding-bottom: ${({ theme }) => theme.space[2]}px;
+  font-weight: ${({ theme }) => theme.fontWeight.medium};
+  border-bottom: 1px solid ${({ theme }) => theme.colors.borderBottom};
+`;
