8767cc8f4931dbd56a3442cc9f9899865311e40a
Vira Voronova <vera.voronova@hotmail.com>
2023-01-27 17:13:46 +0100

    loader position fixed

--- src/components/loader/Loader.tsx
+++ src/components/loader/Loader.tsx
@@ -1,5 +1,6 @@
 import { theme } from 'constants/theme';
 import BarLoader from 'react-spinners/BarLoader';
+import { Wrapper } from './Loader.styled';
 
 const override = {
   display: 'block',
@@ -13,13 +14,15 @@ interface IProps {
 }
 
 const Loader: React.FC<IProps> = ({ isLoading }) => (
-  <BarLoader
-    color={color}
-    loading={isLoading}
-    cssOverride={override}
-    width={160}
-    aria-label="Loading Spinner"
-  />
+  <Wrapper>
+    <BarLoader
+      color={color}
+      loading={isLoading}
+      cssOverride={override}
+      width={160}
+      aria-label="Loading Spinner"
+    />
+  </Wrapper>
 );
 
 export default Loader;
